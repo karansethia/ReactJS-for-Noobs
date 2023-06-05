d18f3b6280c1885a71afc1c87502278fdc108746
Karan <karansethia24@gmail.com>
2023-06-06 00:31:14 +0530

    Adding portals branch

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 
 import styles from './ErrorModal.module.css'
 
+const Backdrop = props => {
+  return <div className={styles.backdrop} onClick={props.onHandleError}/>
+}
+const ErrorCard = props => {
+  return (<Card className={styles.modal}>
+  <header className={styles.header}>
+      <h2>{props.title}</h2>
+  </header>
+  <div className={styles.content}>
+      <p>{props.desc}</p>
+  </div>
+  <footer className={styles.actions}>
+      <Button onClick={props.onHandleError}>Okay</Button>
+  </footer>
+</Card>);
+}
+
 const ErrorModal = (props) => {
   return (
-    <div>
-    <div className={styles.backdrop} onClick={props.onHandleError}/>
-    <Card className={styles.modal}>
-        <header className={styles.header}>
-            <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-            <p>{props.desc}</p>
-        </div>
-        <footer className={styles.actions}>
-            <Button onClick={props.onHandleError}>Okay</Button>
-        </footer>
-    </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop onHandleError={props.onHandleError}/>,document.getElementById('backdrop'))}
+      {ReactDOM.createPortal(<ErrorCard  title={props.title} desc={props.desc} onHandleError={props.onHandleError}/>,document.getElementById('error-card'))}
+    </React.Fragment>
   )
 }
 
