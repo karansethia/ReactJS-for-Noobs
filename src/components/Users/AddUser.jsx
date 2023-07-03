c06d07ee8c2bd6c7bd305141708dc9d5ed631bd6
Karan <karansethia24@gmail.com>
2023-07-03 19:21:30 +0530

    useRef branch

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,64 +1,80 @@
-import React, { useState } from 'react'
-import styles from './AddUser.module.css'
-import logo from './logo.png'
+import React, {useState, useRef} from "react";
+import styles from "./AddUser.module.css";
+import logo from "./logo.png";
 
-import Card from '../UI/Card'
-import Button from '../UI/Button'
-import ErrorModal from '../UI/ErrorModal'
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = props => {
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  const [error, setError] = useState();
 
-    const [name,setName] = useState('');
-    const [age, setAge] = useState('');
-    const [error, setError] = useState();
-
-    const addUserHandler = event => {
-        event.preventDefault();
-        if (name.trim().length === 0 || age.trim().length === 0) {
-            setError({
-                title: 'Invalid Name and age',
-                desc: 'Please enter a valid name and age'
-            });
-            return;
-          }
-          if (+age < 1) {
-            setError({
-                title: 'Invalid Age',
-                desc: 'Please enter a valid age ,Age should be greater than 0'
-            });
-            return;
-          }
-        props.onAddUser(name,(age*525600));
-        setAge('');
-        setName('');
-
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    // console.log(nameInputRef); // !Prints the entire dom node
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Name and age",
+        desc: "Please enter a valid name and age",
+      });
+      return;
     }
-    
-    const setUserHandler = event => {
-        setName(event.target.value);
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid Age",
+        desc: "Please enter a valid age ,Age should be greater than 0",
+      });
+      return;
     }
-    const setAgeHandler = event => {
-        setAge(event.target.value);
-    }
-    const errorHandler = () => {
-        setError(null);
-    }
+    props.onAddUser(enteredName, enteredAge * 525600);
+    /*
+    ? To empty the input tags you can just emplty the current.value like below
+    ? nameInputRef.current.value = ''
+    ! But it is not recommended to access and manipulate dom directly via this method, 
+    ! that is why use 'useref' in case you just want to read a value and donot plan
+    ! on changing it
+    */
+
+
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
 
   return (
     <div>
-    {error && <ErrorModal title={error.title} desc={error.desc} onHandleError={errorHandler}/>}
-    <Card className={styles.input}>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          desc={error.desc}
+          onHandleError={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
         <img src={logo} alt="" />
         <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Name</label>
-            <input type="text" id="username" onChange={setUserHandler} value={name}/>
-            <label htmlFor="age">Age (in years)</label>
-            <input type="number" id="age" onChange={setAgeHandler} value={age}/>
-            <Button type="submit">Calculate</Button>
+          <label htmlFor="username">Name</label>
+          <input
+            type="text"
+            id="username"
+            ref={nameInputRef}
+          />
+          <label htmlFor="age">Age (in years)</label>
+          <input
+            type="number"
+            id="age"
+            ref={ageInputRef}
+          />
+          <Button type="submit">Calculate</Button>
         </form>
-    </Card>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
 export default AddUser;
